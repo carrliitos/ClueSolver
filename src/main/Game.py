88c16b0170f898b65f180738bf74cff9a30557f9
carrliitos <salazar.ten80@gmail.com>
2020-11-04 22:28:07 -0600

    added Player class

---
+++ src/main/Game.py
@@ -0,0 +1,32 @@
+from . import Cards
+from .Exceptions import ImpossibleError
+
+# Game class
+class Game:
+	'''
+	Provide a list of players and cards.
+	Each player should be a (name, # of cards) tuple
+	'''
+	numPlayerCards = len(cards.DECK) - 3
+
+	def __init__(self, me, others):
+		self.others = others
+		self.othersByName = {p.name for p in others}
+		
+		if len(self.othersByName) != len(self.others):
+			raise ImpossibleError("Player names must be unique!")
+
+		self.me = me
+		totalCards = self.me.numCards + sum([p.numCards for p in self.others])
+		if totalCards != self.numPlayerCards:
+			raise ImpossibleError(
+				"%s cards held by players should be %s."
+				%(totalCards, self.numPlayerCards)
+				)
+
+	def snapshot(self):
+		return self.__class__(self.me.snapshot(), 
+			[p.snapshot() for p in self.others])
+
+	def __repr__(self):
+		return "%s(%r, %r)" % (self.__class__.__name__, self.me, self.others)
---
+++ src/main/Player.py
@@ -0,0 +1,78 @@
+from . import Cards
+from .Exceptions import ImpossibleError
+
+# Player class
+class Player:
+	def __init__(self, name, numCards):
+		self.name = name
+		# number of cards this player holds
+		self.numCards = numCards
+		# Set of cards this player is known to have
+		self.hasCards = set()
+		# Set of cards this player is known NOT to have
+		self.notHasCards = set()
+		# list of triple as about
+		self.asked = []
+		# list of triples of which one has been shown to someone
+		self.shown = []
+
+	def hasCard(self, card):
+		sef.hasCards.add(card)
+		if len(self.hasCards) > self.numCards:
+			raise ImpossibleError(
+				"%s has only %s cards but has shwon %r" % 
+				(self.name, self.numCards, self.hasCards))
+
+	def ask(self, triple):
+		self.asked.append(triple)
+
+	def show(self, triple):
+		if set(triple.allCards).issubet(self.notHasCards):
+			raise ImpossibleError(
+				"%s showed me one of %s, but earlier decline to show all those."
+				% (self.name, triple))
+
+		self.shown.append(triple)
+
+	def noshow(self, triple):
+		has = self.hasCards.intersection(triple.allCards)
+		if has:
+			raise ImpossibleError(
+				"%s declined for %s, but we know they have %s"
+				% (self.name, triple.allCards, has))
+
+		self.notHasCards.update(triple.allCards)
+
+	def check(self):
+		impossible = self.hasCards.intersection(self.notHasCards)
+		if impossible:
+			raise ImpossibleError(
+				"%s is known to have and known to not have %s"
+				% (self.name, impossible))
+
+	def snapshot(self):
+		s = self.__class__(self.name, self.numCards)
+		s.hasCards = self.hasCards.copy()
+		s.notHasCards = self.notHasCards.copy()
+		s.asked = self.asked.copy()
+		s.shown = self.shown.copy()
+		return s
+
+	def __repr__(self):
+		return "%s(%r, %r)" % (self.__class__.__name__, self.name, self.numCards)
+
+class Me(Player):
+	def __init__(self, myCards):
+		myCards = set(myCards)
+		super().__init__("Me", len(myCards))
+		self.hasCards.update(myCards)
+		self.notHasCards = set(cards.DECK).difference(self.hasCards)
+
+	def snapshot(self):
+		s = self.__class__(self.hasCards)
+		s.asked = self.asked.copy()
+		s.shown = self.shown.copy()
+		return s
+
+	def __repr__(self):
+		return "%s(%r)" % (self.__class__.__name__, sorted(self.hasCards))
