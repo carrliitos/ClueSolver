32fbfb12d6f547d72c8eac94c77fb26c41a60a26
carrliitos <salazar.ten80@gmail.com>
2020-11-04 22:58:31 -0600

    started adding test cases

---
+++ src/tests/smartcov.py
@@ -0,0 +1,11 @@
+def cov_source_for_test_paths(paths):
+	'''Given test paths, return the source paths on which to measure
+	the coverage. If no specific test paths were given, measure coverage on
+	entier Python code tree.'''
+	if not in paths:
+		return ['code']
+	return [_code_path(p) for p in paths]
+
+def _code_path(path):
+	'''Returns the code path corresponding to a given test path.'''
+	return path.replace('tests/', 'clue/').replace('test_', '')
